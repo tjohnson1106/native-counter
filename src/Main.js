8f0c5815f29a91d14b5c5409a272062820e73ef6
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-10-03 22:05:34 -0400

    created table and form

---
+++ src/Main.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+
+import EditableTimer from "./components/EditableTimer";
+import ToggleableTimerForm from "";
+
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+          <ScrollView style={styles.timerList}>
+            <ToggleableTimerForm isOpen={false} />
+            <EditableTimer
+              id="1"
+              title="Mow the lawn"
+              project="House Chores"
+              elapsed="8986300"
+              isRunning
+            />
+            <EditableTimer
+              id="2"
+              title="Bake squash"
+              project="Kitchen Chores"
+              elapsed="3890985"
+              editFormOpen
+            />
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D6D7DA"
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  timerList: {
+    paddingBottom: 15
+  }
+});
+
+export default Main;
